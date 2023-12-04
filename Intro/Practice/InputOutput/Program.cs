52ace23fb9c58837dd52283b3fe274d63982fc7b
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-04 22:32:26 +0300

    Добавить средний и итоговый балл по нажатию клавиши
    Задание 2

--- Intro/Practice/InputOutput/Program.cs
+++ Intro/Practice/InputOutput/Program.cs
@@ -19,8 +19,17 @@
             string physicScoreFieldName = "Физика:";
             double physicScore = 87.7;
 
-            string line = new string('-', 54);
+            string totalFieldName = "Итоговый балл:";
+            double total = programmingScore + mathScore + physicScore;
 
+            string averageFieldName = "Средний балл:";
+            double average = total / 3;
+
+            string line = new('-', 54);
+            string gridLine = new('=', 54);
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(
                 $"{line}\n" +
                 $"|{fullNameFieldName,-20}| {fullName,-30}|\n" +
@@ -34,8 +43,18 @@
                 $"|{mathScoreFieldName,-20}| {mathScore,-30:0.0}|\n" +
                 $"{line}\n" +
                 $"|{physicScoreFieldName,-20}| {physicScore,-30:0.0}|\n" +
-                $"{line}\n")
+                $"{line}");
+            Console.ReadKey();
+            (int left, int top) = Console.GetCursorPosition();
+            Console.SetCursorPosition(left, top - 1);
+            Console.WriteLine(
+                $"{gridLine}\n" +
+                $"|{totalFieldName,-20}| {total,-30:0.0}|\n" +
+                $"{line}\n" +
+                $"|{averageFieldName,-20}| {average,-30:0.00}|\n" +
+                $"{line}")
                 ;
+            Console.ResetColor();
             Console.ReadKey();
         }
     }
