1d9a0b61495683fc89005d1c777b967c252d1964
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-07 10:06:36 +0300

    Определение четности числа
    Задание 1

---
+++ Intro/Practice/Loops/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Loops {
+    internal class Program {
+        internal static void Main() {
+            EvenOddNumber();
+        }
+
+        internal static void EvenOddNumber() {
+            Console.WriteLine("Введите целое число:");
+            string str = Console.ReadLine() ?? string.Empty;
+            if (int.TryParse(str, out int number)) {
+                Console.WriteLine(number % 2 == 0 ? "Число четное" : "Нечетное число");
+            } else {
+                Console.WriteLine("Неправильный ввод!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
