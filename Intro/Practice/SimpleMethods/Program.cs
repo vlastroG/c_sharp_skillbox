d0a1903d7cdc4a843a225e95830e54a05a9ea1ab
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-11 19:47:53 +0300

    Разделение строки на слова
    Задание 1

---
+++ Intro/Practice/SimpleMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleMethods {
+    internal class Program {
+        internal static void Main(string[] args) {
+            WriteWords(SplitText(GetStringFromUser("Введите несколько слов, разделенных пробелом:"))); //Задание 1
+        }
+
+
+        private static string[] SplitText(string? text) {
+            return text?.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+        }
+
+        private static void WriteWords(string[] words) {
+            Array.ForEach(words, (words) => Console.WriteLine(string.Join("\n", words)));
+        }
+
+        private static string GetStringFromUser(string msg) {
+            Console.WriteLine($"{msg}");
+            return Console.ReadLine() ?? string.Empty;
+        }
+    }
+}
