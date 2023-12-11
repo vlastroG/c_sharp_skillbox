493b2ee9edcde8f928f7d6006a4b8964f4732ad4
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-11 20:01:05 +0300

    Перестановка слов в предложении
    Задание 2

--- Intro/Practice/SimpleMethods/Program.cs
+++ Intro/Practice/SimpleMethods/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace SimpleMethods {
     internal class Program {
         internal static void Main(string[] args) {
-            WriteWords(SplitText(GetStringFromUser("Введите несколько слов, разделенных пробелом:"))); //Задание 1
+            //WriteWords(SplitText(GetStringFromUser("Введите несколько слов, разделенных пробелом:"))); //Задание 1
+            ReversWords(GetStringFromUser("Введите фразу, слова которой нужно написать в обратном порядке:"));//Задание 2
         }
 
 
@@ -16,6 +17,14 @@
         private static string GetStringFromUser(string msg) {
             Console.WriteLine($"{msg}");
             return Console.ReadLine() ?? string.Empty;
+        }
+
+        private static string Reverse(string text) {
+            return string.Join(' ', SplitText(text).Reverse());
+        }
+
+        private static void ReversWords(string inputPhrase) {
+            Console.WriteLine(Reverse(inputPhrase));
         }
     }
 }
