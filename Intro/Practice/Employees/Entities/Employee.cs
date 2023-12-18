3b95e9af7caab1ad88145d92e3f6b3a936dd280e
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-18 15:11:47 +0300

    Работа со StreamReader и StreamWriter

---
+++ Intro/Practice/Employees/Entities/Employee.cs
@@ -0,0 +1,57 @@
+﻿namespace Employees.Entities {
+    internal class Employee {
+        private static long _lastId = 1;
+
+
+        public Employee(string surname, string name, string patronymic) {
+            if (string.IsNullOrWhiteSpace(name)) {
+                throw new ArgumentException($"'{nameof(name)}' cannot be null or whitespace.", nameof(name));
+            }
+
+            if (string.IsNullOrWhiteSpace(surname)) {
+                throw new ArgumentException($"'{nameof(surname)}' cannot be null or whitespace.", nameof(surname));
+            }
+
+            if (string.IsNullOrWhiteSpace(patronymic)) {
+                throw new ArgumentException($"'{nameof(patronymic)}' cannot be null or whitespace.", nameof(patronymic));
+            }
+
+            ID = _lastId++;
+            RecordDate = DateTime.Now;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+        }
+
+
+        public long ID { get; }
+
+        public DateTime RecordDate { get; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Surname { get; set; } = string.Empty;
+
+        public string Patronymic { get; set; } = string.Empty;
+
+        public int Age {
+            get {
+                var now = DateTime.Now;
+                return (now.Year - Birthday.Year)
+                    + ((Birthday.Month > now.Month)
+                    || (Birthday.Month == now.Month && Birthday.Day > now.Day) ? (-1) : 0);
+            }
+        }
+
+        public DateTime Birthday { get; set; }
+
+        public string HomeCity { get; set; } = string.Empty;
+
+        public double Height { get; set; }
+
+
+        public override string ToString() {
+            return $"{ID}#{RecordDate:dd.MM.yyyy HH:mm}#{Surname} {Name} {Patronymic}#{Age}#{Height}#{Birthday:dd.MM.yyyy}#{HomeCity}";
+        }
+    }
+}
---
+++ Intro/Practice/Employees/Repositories/EmployeesRepository.cs
@@ -0,0 +1,38 @@
+﻿using Employees.Entities;
+using System.Text;
+
+namespace Employees.Repositories {
+    internal class EmployeesRepository {
+        private const string _path = "./employees.txt";
+
+
+        public EmployeesRepository() {
+
+        }
+
+
+        public void Add(Employee employee) {
+            using (StreamWriter writer = new StreamWriter(_path, true)) {
+
+                writer.WriteLine(employee.ToString());
+            }
+        }
+
+
+        public string GetAll() {
+            if (File.Exists(_path)) {
+                using (StreamReader reader = new StreamReader(_path)) {
+                    StringBuilder sb = new StringBuilder();
+                    string? text;
+                    while ((text = reader.ReadLine()) != null) {
+                        sb.Append(text);
+                        sb.Append(Environment.NewLine);
+                    }
+                    return sb.ToString();
+                }
+            } else {
+                return "Данные по сотрудникам отсутствуют.";
+            }
+        }
+    }
+}
