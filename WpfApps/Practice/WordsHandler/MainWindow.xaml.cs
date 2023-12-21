64a5776c30bfeb7c47cf8068981076433569ba34
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-21 16:15:52 +0300

    Добавить приложение для обработки строк

---
+++ WpfApps/Practice/WordsHandler/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using WordsHandler.ViewModels;
+
+namespace WordsHandler {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
