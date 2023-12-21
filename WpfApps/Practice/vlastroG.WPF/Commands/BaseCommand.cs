08d23d8d02d49cd45cfce59d8a950d1d6b684652
Vladimir Stroganov <vlastroganov@gmail.com>
2023-12-21 16:15:35 +0300

    Добавить базовые классы LambdaCommand и BaseViewModel

---
+++ WpfApps/Practice/vlastroG.WPF/Commands/BaseCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace vlastroG.WPF.Commands {
+    public abstract class BaseCommand : ICommand {
+        protected BaseCommand() { }
+
+
+        public event EventHandler? CanExecuteChanged {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// Определяет, можно ли выполнить команду
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public abstract bool CanExecute(object? parameter);
+
+        /// <summary>
+        /// Выполняет действие команды
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object? parameter);
+    }
+}
