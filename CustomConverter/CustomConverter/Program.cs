955533c6eefd7dd797a4f3878ae89629494248ac
Vladimir Stroganov <vlastroganov@gmail.com>
2024-04-19 17:04:56 +0300

    Добавить сравнение с double.Parse

--- CustomConverter/CustomConverter/Program.cs
+++ CustomConverter/CustomConverter/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace CustomConverter
+﻿using System.Globalization;
+
+namespace CustomConverter
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             double number = DoubleConverter.Convert(args[0]);
-            Console.WriteLine(number);
+            Console.WriteLine($"My           --> {number}");
+            Console.WriteLine($"double.Parse --> {double.Parse(args[0], CultureInfo.InvariantCulture)}");
         }
     }
 }
